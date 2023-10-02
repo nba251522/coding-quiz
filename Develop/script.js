2c3ade0f819c7b1d9070ba59bd1b966821588b54
nba251522 <inquister112345678@gmail.com>
2023-10-01 21:13:53 -0700

    added functions and variables to my js. I also fixed the path on my index.html so that it was properly linking my js

--- Develop/script.js
+++ Develop/script.js
@@ -1,95 +1,64 @@
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specialCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+var timer = 60;
+var currentQuestion = 0;
+var score = 0;
 
-function getRandomCharacter(string) {
-    return string.charAt(Math.floor(Math.random() * string.length));
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Transfer Markup Language"],
+        correct: 1
+    },
+];
+
+document.getElementById('start-btn').addEventListener('click', function() {
+    this.style.display = 'none';
+    document.getElementById('question-container').style.display = 'block';
+    startTimer();
+    displayQuestion();
+});
+
+function startTimer() {
+    var timerInterval = setInterval(function() {
+        timer--;
+        document.getElementById('time-value').innerText = timer;
+
+        if (timer <= 0 || currentQuestion >= questions.length) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+    }, 1000);
+}
+function displayQuestion() {
+    var q = questions[currentQuestion];
+    var container = document.getElementById('answer-buttons');
+    container.innerHTML = '';
+    document.getElementById('question-text').innerText = q.question;
+
+    for (var i = 0; i < q.answers.length; i++) {
+        (function(index) {
+            var btn = document.createElement('button');
+            btn.innerText = q.answers[index];
+            btn.addEventListener('click', function(e) {
+                if (index === q.correct) {
+                    score++;
+                } else {
+                    timer -= 10; 
+                }
+    
+                if (currentQuestion < questions.length - 1) {
+                    currentQuestion++;
+                    displayQuestion();
+                } else {
+                    endGame();
+                }
+            });
+            container.appendChild(btn);
+        })(i);
+    }
+}
+function endGame() {
+    document.getElementById('question-container').style.display = 'none';
+    document.getElementById('score-container').style.display = 'block';
+    document.getElementById('score-value').innerText = score;
 }
 
-function shuffleString(string) {
-    return string.split('').sort(function() { return 0.5 - Math.random() }).join('');
-}
-
-function generatePassword() {
-    var generatedPassword = "";
-    var passwordLength, includeLowercase, includeUppercase, includeNumber, includeSpecialCharacter;
-    var numLowercase = 0, numUppercase = 0, numNumbers = 0, numSpecialCharacters = 0;
-
-    while (true) {
-        passwordLength = prompt("Enter password length between 8 and 128:");
-        if (passwordLength >= 8 && passwordLength <= 128) {
-            break;
-        }
-        alert("Password length must be between 8 and 128. Please enter a valid length.");
-    }
-
-    while (true) {
-        includeLowercase = confirm("Include lowercase characters?");
-        if (includeLowercase) {
-            numLowercase = parseInt(prompt("Please enter the desired amount of lower case letters"));
-        }
-
-        includeUppercase = confirm("Include uppercase characters?");
-        if (includeUppercase) {
-            numUppercase = parseInt(prompt("Please enter the desired amount of upper case letters"));
-        }
-
-        includeNumber = confirm("Include numbers?");
-        if (includeNumber) {
-            numNumbers = parseInt(prompt("Please enter the desired amount of numbers"));
-        }
-
-        includeSpecialCharacter = confirm("Include special characters?");
-        if (includeSpecialCharacter) {
-            numSpecialCharacters = parseInt(prompt("Please enter the desired amount of special characters"));
-        }
-
-        if (!includeLowercase && !includeUppercase && !includeNumber && !includeSpecialCharacter) {
-            alert("You must select at least one character type.");
-            continue;
-        }
-
-        var totalCharacters = numLowercase + numUppercase + numNumbers + numSpecialCharacters;
-
-        if (totalCharacters === parseInt(passwordLength)) {
-            break;
-        } else {
-            alert("The total number of characters (" + totalCharacters + ") does not match the password length (" + passwordLength + "). Please try again.");
-        }
-    }
-
-    for (var i = 0; i < numLowercase; i++) {
-        generatedPassword += getRandomCharacter(lowercase);
-    }
-
-    for (var i = 0; i < numUppercase; i++) {
-        generatedPassword += getRandomCharacter(uppercase);
-    }
-
-    for (var i = 0; i < numNumbers; i++) {
-        generatedPassword += getRandomCharacter(numbers);
-    }
-
-    for (var i = 0; i < numSpecialCharacters; i++) {
-        generatedPassword += getRandomCharacter(specialCharacters);
-    }
-
-    generatedPassword = shuffleString(generatedPassword);
-
-    return generatedPassword;
-}
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
